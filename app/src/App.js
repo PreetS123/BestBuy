f15804d434f7ff0d6429b350079c1a0fbdad2e02
PreetS123 <preeti_fw15_365@masai.school>
2022-06-12 15:05:37 +0530

    navbar_ready

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 
 // import './App.css';
 import { Footer } from './Components/Footer';
+import { Navbar } from './Components/Navbar';
 import { Home } from './LandingPage/Home';
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
      {/* <h1>Girl</h1> */}
+     <Navbar/>
        <Home/>
        <Footer/>
     </div>
