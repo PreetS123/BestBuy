4d38f7cb6e3ecc01483bd05c94e1f44176dea6ae
PreetS123 <preeti_fw15_365@masai.school>
2022-06-15 06:56:14 +0530

    login_structure_complete

--- app/src/Signup_Login/Login.jsx
+++ app/src/Signup_Login/Login.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Flex,
   Box,
@@ -6,33 +7,37 @@ import {
   Input,
   Checkbox,
   Stack,
-  Link,
   Button,
   Heading,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
+  Divider,
+} from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+import { FaApple,  FaFingerprint } from "react-icons/fa";
+
+import { Center } from "@chakra-ui/react";
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-          </Text>
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to Best Buy</Heading>
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={2}>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
               <Input type="email" />
@@ -41,26 +46,93 @@ export default function Login() {
               <FormLabel>Password</FormLabel>
               <Input type="password" />
             </FormControl>
-            <Stack spacing={10}>
+            <Stack spacing={8}>
               <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
                 <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
+                <Link to='#' color={"blue.400"}>Forgot password?</Link>
               </Stack>
               <Button
-                bg={'blue.400'}
-                color={'white'}
+                bg={"blue.400"}
+                color={"white"}
                 _hover={{
-                  bg: 'blue.500',
-                }}>
+                  bg: "blue.500",
+                }}
+              >
                 Sign in
               </Button>
             </Stack>
           </Stack>
+          <Divider marginTop={5} border={"1px solid"} />
+          {/* <Stack w={"100%"}> */}
+            <All />
+          {/* </Stack> */}
         </Box>
       </Stack>
     </Flex>
+  );
+}
+
+function All() {
+  return (
+   
+      <Stack spacing={2} >
+        {/* Facebook */}
+        <Button 
+          bg={"white.400"}
+          color={"blue"}
+          _hover={{
+            color: "white",
+            bg: "blue",
+          }}
+          variant={"outline"}
+          leftIcon={<FaFingerprint />}
+        >
+          <Center>
+            <Text>Sign in with WebAuth</Text>
+          </Center>
+        </Button>
+
+        {/* Google */}
+        <Button
+          bg={"white.400"}
+          color={"blue"}
+          _hover={{
+            color: "white",
+            bg: "blue",
+          }}
+          variant={"outline"}
+          leftIcon={<FcGoogle />}
+        >
+          <Center>
+            <Text>Sign in with Google</Text>
+          </Center>
+        </Button>
+
+        {/* Apple */}
+        <Button
+          bg={"white.400"}
+          color={"black"}
+          _hover={{
+            color: "white",
+            bg: "black",
+          }}
+          variant={"outline"}
+          leftIcon={<FaApple />}
+        >
+          <Center>
+            <Text>Sign in with Apple</Text>
+          </Center>
+        </Button>
+        <Text color={'gray.500'}>By continuing you agree to our Terms and Conditions, our Privacy Policy, and the My Best Buy® Program Terms.</Text>
+        <Flex align={'center'}>
+          <Heading fontSize={'16px'} >Don't have an account?</Heading>
+          <Link to='/signup'>Create an account</Link>
+        </Flex>
+      </Stack>
+    
   );
 }
