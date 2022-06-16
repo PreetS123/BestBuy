16132666b13f90ea87f616a56b07ab4337273fd4
PreetS123 <preeti_fw15_365@masai.school>
2022-06-16 10:42:36 +0530

    authentication_done

--- app/src/Signup_Login/Login.jsx
+++ app/src/Signup_Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Flex,
   Box,
@@ -14,12 +14,40 @@ import {
   Divider,
 } from "@chakra-ui/react";
 import { FcGoogle } from "react-icons/fc";
-import { FaApple,  FaFingerprint } from "react-icons/fa";
+import { FaApple, FaFingerprint } from "react-icons/fa";
 
 import { Center } from "@chakra-ui/react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const [flag, setFlag]=useState(false);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  console.log(formData);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  };
+  const handleSubmit = async () => {
+    const data = await fetch("https://login-backend1.herokuapp.com/users")
+    .then(
+      (data) => data.json()
+    );
+    data.map((e) => {
+      if (e.email === formData.email && e.password === formData.pass) {
+      //  let identify = e.firstname;
+        navigate("/");
+      }
+      else{
+        setFlag(!flag);
+      }
+    });
+  };
   return (
     <Flex
       minH={"100vh"}
@@ -40,11 +68,11 @@ export default function Login() {
           <Stack spacing={2}>
             <FormControl id="email">
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input type="email" name="email" onChange={handleChange} />
             </FormControl>
             <FormControl id="password">
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
+              <Input type="password" name="pass" onChange={handleChange} />
             </FormControl>
             <Stack spacing={8}>
               <Stack
@@ -53,7 +81,9 @@ export default function Login() {
                 justify={"space-between"}
               >
                 <Checkbox>Remember me</Checkbox>
-                <Link to='#' color={"blue.400"}>Forgot password?</Link>
+                <Link to="#" color={"blue.400"}>
+                  Forgot password?
+                </Link>
               </Stack>
               <Button
                 bg={"blue.400"}
@@ -61,14 +91,22 @@ export default function Login() {
                 _hover={{
                   bg: "blue.500",
                 }}
+                onClick={handleSubmit}
               >
                 Sign in
               </Button>
             </Stack>
+            <Box>
+              {flag ? (
+                <Text>
+                  Looks like your email or passwordis incorrect . Try Again{" "}
+                </Text>
+              ) : null}
+            </Box>
           </Stack>
           <Divider marginTop={5} border={"1px solid"} />
           {/* <Stack w={"100%"}> */}
-            <All />
+          <All />
           {/* </Stack> */}
         </Box>
       </Stack>
@@ -78,61 +116,62 @@ export default function Login() {
 
 function All() {
   return (
-   
-      <Stack spacing={2} >
-        {/* Facebook */}
-        <Button 
-          bg={"white.400"}
-          color={"blue"}
-          _hover={{
-            color: "white",
-            bg: "blue",
-          }}
-          variant={"outline"}
-          leftIcon={<FaFingerprint />}
-        >
-          <Center>
-            <Text>Sign in with WebAuth</Text>
-          </Center>
-        </Button>
+    <Stack spacing={2}>
+      {/* Facebook */}
+      <Button
+        bg={"white.400"}
+        color={"blue"}
+        _hover={{
+          color: "white",
+          bg: "blue",
+        }}
+        variant={"outline"}
+        leftIcon={<FaFingerprint />}
+      >
+        <Center>
+          <Text>Sign in with WebAuth</Text>
+        </Center>
+      </Button>
 
-        {/* Google */}
-        <Button
-          bg={"white.400"}
-          color={"blue"}
-          _hover={{
-            color: "white",
-            bg: "blue",
-          }}
-          variant={"outline"}
-          leftIcon={<FcGoogle />}
-        >
-          <Center>
-            <Text>Sign in with Google</Text>
-          </Center>
-        </Button>
+      {/* Google */}
+      <Button
+        bg={"white.400"}
+        color={"blue"}
+        _hover={{
+          color: "white",
+          bg: "blue",
+        }}
+        variant={"outline"}
+        leftIcon={<FcGoogle />}
+      >
+        <Center>
+          <Text>Sign in with Google</Text>
+        </Center>
+      </Button>
 
-        {/* Apple */}
-        <Button
-          bg={"white.400"}
-          color={"black"}
-          _hover={{
-            color: "white",
-            bg: "black",
-          }}
-          variant={"outline"}
-          leftIcon={<FaApple />}
-        >
-          <Center>
-            <Text>Sign in with Apple</Text>
-          </Center>
-        </Button>
-        <Text color={'gray.500'}>By continuing you agree to our Terms and Conditions, our Privacy Policy, and the My Best Buy® Program Terms.</Text>
-        <Flex align={'center'}>
-          <Heading fontSize={'16px'} >Don't have an account?</Heading>
-          <Link to='/signup'>Create an account</Link>
-        </Flex>
-      </Stack>
-    
+      {/* Apple */}
+      <Button
+        bg={"white.400"}
+        color={"black"}
+        _hover={{
+          color: "white",
+          bg: "black",
+        }}
+        variant={"outline"}
+        leftIcon={<FaApple />}
+      >
+        <Center>
+          <Text>Sign in with Apple</Text>
+        </Center>
+      </Button>
+      <Text color={"gray.500"}>
+        By continuing you agree to our Terms and Conditions, our Privacy Policy,
+        and the My Best Buy® Program Terms.
+      </Text>
+      <Flex align={"center"}>
+        <Heading fontSize={"16px"}>Don't have an account?</Heading>
+        <Link to="/signup">Create an account</Link>
+      </Flex>
+    </Stack>
   );
 }
--- app/src/Signup_Login/SignUp.jsx
+++ app/src/Signup_Login/SignUp.jsx
@@ -1,30 +1,58 @@
-import { Box, Button, Center, Divider, Flex, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { FcGoogle } from 'react-icons/fc';
-
+import { FcGoogle } from "react-icons/fc";
 
 export const SignUp = () => {
-  const [Name, setName] = useState("");
-  const [userMail, setuserMail] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  const [userMobile, setUserMobile] = useState("");
-
-  const handleSubmit = (e) => {
-    let userData = {
-      name: Name,
-      email: userMail,
-      password: userPassword,
-      mobile: userMobile,
-    };
-    console.log(userData);
+  const navigate = useNavigate();
+  const [data, setData] = useState({});
+  console.log("SignUpData:", data);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    })
   };
+  const handleSubmit=()=>{
+    fetch("https://login-backend1.herokuapp.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    alert("Account Created");
+    navigate("/login");
+  }
 
   return (
-    <Flex   w={"40%"} margin='100px auto' alignItems="center" justifyContent={"center"}>
-      <Flex border={'1px solid'} p={4} borderColor='rgb(196,196,196)' direction={"column"} h="600px" justifyContent="space-around">
-        <Text fontWeight={700} fontSize={30} textAlign={'center'}>Create an Account</Text>
+    <Flex
+      w={"40%"}
+      margin="100px auto"
+      alignItems="center"
+      justifyContent={"center"}
+    >
+      <Flex
+        border={"1px solid"}
+        p={4}
+        borderColor="rgb(196,196,196)"
+        direction={"column"}
+        h="600px"
+        justifyContent="space-around"
+      >
+        <Text fontWeight={700} fontSize={30} textAlign={"center"}>
+          Create an Account
+        </Text>
         <Text>
           Shopping for your business?
           <Link to="#">Create a bussiness account.</Link>
@@ -33,24 +61,30 @@ export const SignUp = () => {
         <Box>
           <Input
             placeholder="First Name"
-            onChange={(e) => setName(e.target.value)}
+            onChange={handleChange}
+            name="firstname"
+            type="text"
             required
           />
         </Box>
         <Box>
-          <Input placeholder="Last Name" required />
+          <Input placeholder="Last Name" name="lastname" type="text" required />
         </Box>
         <Box>
           <Input
             placeholder="Email Address"
-            onChange={(e) => setuserMail(e.target.value)}
+            onChange={handleChange}
+            name="email"
+            type="email"
             required
           />
         </Box>
         <Box>
           <Input
             placeholder="Password"
-            onChange={(e) => setUserPassword(e.target.value)}
+            onChange={handleChange}
+            name="password"
+            type="password"
             required
           />
         </Box>
@@ -60,7 +94,9 @@ export const SignUp = () => {
         <Box>
           <Input
             placeholder="Mobile Phone Number"
-            onChange={(e) => setUserMobile(e.target.value)}
+            onChange={handleChange}
+            name="number"
+            type="number"
             required
           />
         </Box>
@@ -70,7 +106,7 @@ export const SignUp = () => {
               w={"full"}
               maxW={"md"}
               colorScheme={"messenger"}
-              onClick={() => handleSubmit()}
+              onClick={ handleSubmit}
             >
               <Center>
                 <Text>SUBMIT </Text>
@@ -78,8 +114,8 @@ export const SignUp = () => {
             </Button>
           </Center>
         </Box>
-        <Divider border={'2px'}/>
-        <Button w={'full'} variant={'outline'} leftIcon={<FcGoogle />}>
+        <Divider border={"2px"} />
+        <Button w={"full"} variant={"outline"} leftIcon={<FcGoogle />}>
           <Center>
             <Text>Sign in with Google</Text>
           </Center>
