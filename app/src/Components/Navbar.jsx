6479a5bd2c8a0bf822e4940df1b44351c9a18c73
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-18 06:47:25 +0530

    navbar completed

--- app/src/Components/Navbar.jsx
+++ app/src/Components/Navbar.jsx
@@ -24,6 +24,8 @@ import {
   useDisclosure,
   HStack,
   Heading,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { FaAngleDown, FaArrowRight, FaBars, FaSearch } from "react-icons/fa";
@@ -47,74 +49,102 @@ export const Navbar = () => {
           top: 0,
         }}
       >
-        <HStack w={"90%"}>
-          <Link to="/">
-            <Image
-              boxSize="70px"
-              bg={"none"}
-              src="https://tse1.mm.bing.net/th?id=OIP.7d9ohKK2am7oCloUFeQn4wHaFb&pid=Api&P=0&w=79&h=75"
-              alt="Best Buy"
+        <Flex w={"90%"} gap='10px' justify={'space-between'} align='center'>
+          <HStack flex={1}>
+            <Link to="/">
+              <Image
+                boxSize="70px"
+                bg={"none"}
+                src="https://tse1.mm.bing.net/th?id=OIP.7d9ohKK2am7oCloUFeQn4wHaFb&pid=Api&P=0&w=79&h=75"
+                alt="Best Buy"
+              />
+            </Link>
+            <Button
+              bg={"#1D34C8"}
+              transition={"none"}
+              borderRadius={"none"}
+              color="#fff"
+              fontSize={{ base: 14, sm: 18, md: 18, lg: 20 }}
+              _hover={{
+                bg: "#1D34C8",
+                transition: "none",
+              }}
+              onClick={onOpen}
+            >
+              <FaBars style={{ fontSize: "18px" }} /> Menu
+            </Button>
+          </HStack>
+          <InputGroup flex={3}  size="md" display={{base:'none',md:'block',lg:'block'}}>
+            <Input
+              pr="4.5rem"
+              borderRadius={"0rem"}
+              placeholder="What can we help you find today"
+              bg={"#fff"}
             />
-          </Link>
-          <Button
-            bg={"#1D34C8"}
-            transition={"none"}
-            borderRadius={"none"}
-            color="#fff"
-            fontSize={{ base: 14, sm: 18, md: 18, lg: 20 }}
-            _hover={{
-              bg: "#1D34C8",
-              transition: "none",
-            }}
-            onClick={onOpen}
-          >
-            <FaBars /> Menu
-          </Button>
-        </HStack>
+            <InputRightElement color={"gray.500"} >
+              <FaSearch style={{ fontSize: "1.6rem" }} />
+            </InputRightElement>
+          </InputGroup>
+          <HStack flex={1} float={'right'} justify={'flex-end'}>
+            <Link to="/cart">
+              <Button
+                disabled={cart.length === 0}
+                bg={"#0944B4"}
+                _hover={{
+                  bg: "#0944B4",
+                  translate: "0px",
+                }}
+              >
+                <FaShoppingCart style={{ height: "35px", width: "30px",color:'#fff' }} />
+              </Button>
+            </Link>
+            </HStack>
+        </Flex>
       </Flex>
 
-      <Drawer placement={"left"} onClose={onClose} isOpen={isOpen} >
+      <Drawer placement={"left"} onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
-        <DrawerContent style={{backgroundColor:'#1D34C8' ,color:'#fff'}}>
+        <DrawerContent style={{ backgroundColor: "#1D34C8", color: "#fff" }}>
           <DrawerHeader borderBottomWidth="1px">
-            <Heading fontSize={{base:10,md:14,lg:16}}>
-            <span>BESTBUY</span>{" "}
-              <BsTagFill style={{ color: "yellow" }} />
+            <Heading fontSize={{ base: 10, md: 14, lg: 16 }}>
+              <span>BESTBUY</span> <BsTagFill style={{ color: "yellow" }} />
             </Heading>
           </DrawerHeader>
           <DrawerBody>
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               Deals <FaArrowRight />{" "}
             </Flex>
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               Support & Services <FaArrowRight />
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               Brands <FaArrowRight />{" "}
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               Featured <FaArrowRight />{" "}
             </Flex>{" "}
-            <Flex align="center" h={'50px'}>Shop by Department</Flex>
-            <Divider/>
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex align="center" h={"50px"}>
+              Shop by Department
+            </Flex>
+            <Divider />
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               <Link to="#">Appliances </Link>
               <FaArrowRight />
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               {" "}
               <Link to="/phone">Cell Phones</Link> <FaArrowRight />{" "}
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               <Link to="/tv"> TV & Home Theater</Link> <FaArrowRight />{" "}
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               <Link to="/computer"> Computers & Tablets</Link> <FaArrowRight />{" "}
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               <Link to="/computer"> Computers & Tablets</Link> <FaArrowRight />{" "}
             </Flex>{" "}
-            <Flex alignItems="center" justify={'space-between'} h={'40px'}>
+            <Flex alignItems="center" justify={"space-between"} h={"40px"}>
               {" "}
               <Link to="/camera"> Cameras, Camcarders & Drones </Link>
               <FaArrowRight />{" "}
